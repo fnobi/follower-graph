aa48706093c0675f258be947b1869cfc64b0b11b
Fujisawa Shin <fnobi0.8@gmail.com>
2021-08-05 22:39:47 +0900

    結果はfirestoreに保存

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,22 @@
+import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
 import {TWITTER_BEARER_TOKEN} from "./local/config";
 import TwitterApiClient from "./local/TwitterApiClient";
+
+admin.initializeApp(functions.config().firebase);
+
+const USER_LIST = ["fnobi"];
 
 exports.scheduleFetchFollowers = functions.pubsub
     .schedule("every 5 minutes")
     .onRun(async () => {
       const client = new TwitterApiClient(TWITTER_BEARER_TOKEN);
-      const res = await client.showUser("fnobi");
-      console.log("[followers_count]", res.followers_count);
-      console.log("[friends_count]", res.friends_count);
+      await Promise.all(USER_LIST.map(async (name) => {
+        const res = await client.showUser(name);
+        await admin.firestore().collection(name).doc().set({
+          createdAt: Date.now(),
+          followersCount: res.followers_count,
+          friendsCount: res.friends_count,
+        });
+      }));
     });
