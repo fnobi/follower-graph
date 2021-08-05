b1fa3231e1f792bbfb4b2a791677da57fdf05606
fnobi <fnobi0.8@gmail.com>
2021-08-05 21:50:12 +0900

    ざらっとリクエストサンプル

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,31 @@
 import * as functions from "firebase-functions";
+import got from "got";
+import {TWITTER_BEARER_TOKEN} from "./local/config";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const API_ORIGIN = "https://api.twitter.com";
+
+const callTwitterApi = (
+    path: string,
+    searchParams: {[key:string]:string}
+) => got
+    .get(`${API_ORIGIN}${path}`, {
+      searchParams,
+      headers: {
+        "Authorization": `Bearer ${TWITTER_BEARER_TOKEN}`,
+      },
+    });
+
+const searchRecentTweet = (query:string) =>
+  callTwitterApi(
+      "/2/tweets/search/recent",
+      {
+        query,
+      }
+  ).json();
+
+exports.scheduleFetchFollowers = functions.pubsub
+    .schedule("every 5 minutes")
+    .onRun(async () => {
+      const res = await searchRecentTweet("from:twitterdev");
+      console.log(res);
+    });
---
+++ functions/src/local/config.ts
@@ -0,0 +1,9 @@
+import * as functions from "firebase-functions";
+
+const {
+  bearer_token: TWITTER_BEARER_TOKEN,
+} = functions.config().twitter || {};
+
+export {
+  TWITTER_BEARER_TOKEN,
+};
