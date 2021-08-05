1bb3e8b29793e861bfbc3d15d45616cf8a61b77a
Fujisawa Shin <fnobi0.8@gmail.com>
2021-08-05 23:19:54 +0900

    configそんだけしかいらんのか

--- src/local/firebaseConfig.ts
+++ src/local/firebaseConfig.ts
@@ -1,10 +1,5 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyCycMuhdoanpX8y96sZy614xpY-fcBzeIA",
-  authDomain: "follower-graph.firebaseapp.com",
-  projectId: "follower-graph",
-  storageBucket: "follower-graph.appspot.com",
-  messagingSenderId: "179847923512",
-  appId: "1:179847923512:web:50e10714d6ff52364e23cd"
+  projectId: "follower-graph"
 };
 
 export default firebaseConfig;
