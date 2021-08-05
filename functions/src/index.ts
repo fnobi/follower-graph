31b6727727dfc5057bf9fbe2c1e3114b9c55f0f0
Fujisawa Shin <fnobi0.8@gmail.com>
2021-08-05 22:03:22 +0900

    ととのえる・showで呼ぶ

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,30 +2,53 @@ import * as functions from "firebase-functions";
 import got from "got";
 import {TWITTER_BEARER_TOKEN} from "./local/config";
 
-const API_ORIGIN = "https://api.twitter.com";
 
-const callTwitterApi = (
-    path: string,
-    searchParams: {[key:string]:string}
-) => got
-    .get(`${API_ORIGIN}${path}`, {
-      searchParams,
-      headers: {
-        "Authorization": `Bearer ${TWITTER_BEARER_TOKEN}`,
-      },
-    });
+class TwitterApiClient {
+    private static API_ORIGIN = "https://api.twitter.com";
 
-const searchRecentTweet = (query:string) =>
-  callTwitterApi(
-      "/2/tweets/search/recent",
-      {
-        query,
-      }
-  ).json();
+    private bearerToken: string;
+
+    public constructor(bearerToken: string) {
+        this.bearerToken = bearerToken;
+    }
+
+    private callApi (
+        path: string,
+        searchParams: {[key:string]:string}
+    ) {
+        return got
+            .get(`${TwitterApiClient.API_ORIGIN}${path}`, {
+                searchParams,
+                headers: {
+                    "Authorization": `Bearer ${this.bearerToken}`,
+                },
+            })
+            .json();
+    }
+
+    public searchRecentTweet  (query:string) {
+        return this.callApi(
+            "/2/tweets/search/recent",
+            {
+                query,
+            }
+        );
+    }
+
+    public showUser (screenName:string) {
+        return this.callApi(
+            "/1.1/users/show.json",
+            {
+                screen_name: screenName,
+            }
+        );
+    }
+}
 
 exports.scheduleFetchFollowers = functions.pubsub
     .schedule("every 5 minutes")
     .onRun(async () => {
-      const res = await searchRecentTweet("from:twitterdev");
-      console.log(res);
+        const client = new TwitterApiClient(TWITTER_BEARER_TOKEN);
+        const res = await client.showUser('fnobi');
+        console.log(res);
     });
