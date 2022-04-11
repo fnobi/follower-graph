0cc37eca86e528d70f2a7282c1a4302a581cc01d
Fujisawa Shin <fnobi0.8@gmail.com>
2022-04-11 21:39:11 +0900

    numberのparseしくっとるがな

--- src/scheme/TwitterEntry.ts
+++ src/scheme/TwitterEntry.ts
@@ -15,10 +15,10 @@ export const parseTwitterEntry = (src: any): TwitterEntry => {
     id: String(src.id || ""),
     text: String(src.text || ""),
     createdAt: String(src.createdAt || ""),
-    updatedAt: Number(src.updatedAt),
-    retweetCount: Number(src.retweetCount),
-    replyCount: Number(src.replyCount),
-    likeCount: Number(src.likeCount),
-    quoteCount: Number(src.quoteCount)
+    updatedAt: Number(src.updatedAt || 0),
+    retweetCount: Number(src.retweetCount || 0),
+    replyCount: Number(src.replyCount || 0),
+    likeCount: Number(src.likeCount || 0),
+    quoteCount: Number(src.quoteCount || 0)
   };
 };
